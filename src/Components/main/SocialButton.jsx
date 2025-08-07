3f1391ff70ef02f9c2df6960ca92e08588b8e955
mohd anas <anasstudy01@gmail.com>
2025-08-07 12:44:33 +0530

    Remove SocialButton component as it is no longer needed

--- src/Components/main/SocialButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SocialButton = ({ children }) => (
-  <button className="w-12 h-12 bg-green-500 hover:bg-green-600 text-white rounded-lg flex items-center justify-center flex-shrink-0">
-    <span className="text-lg font-bold">{children}</span>
-  </button>
-)
-
-export default SocialButton
